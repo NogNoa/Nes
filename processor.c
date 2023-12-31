34b7ad7d17415d241911d6971d215f4abc0d2976
NogNoa <53447424+NogNoa@users.noreply.github.com>
2023-12-31 20:01:44 +0200

    debug
    vscode

--- processor.c
+++ processor.c
@@ -1,41 +1,42 @@
 #include <stdint.h>
 
-struct register 
+struct rgistr 
 {	
 	uint16_t PC;
 	uint8_t A;
 	uint8_t X;
 	uint8_t Y;
 	uint8_t SP;
-	union 
+	union
 	{
 		uint8_t full;
 		struct {
-		c: 1;
-		z: 1;
-		i: 1;
-		d: 1;
-		b: 1;
-		_: 1;
-		v: 1;
-		n: 1;
+			unsigned 
+			c: 1,
+			z: 1,
+			i: 1,
+			d: 1,
+			b: 1,
+			_: 1,
+			v: 1,
+			n: 1;
 		} flags;
 	} P;
 };
 
 struct
 {
-	struct register reg;
-	uint8_t[0x10000] memory;
+	struct rgistr reg;
+	uint8_t memory[0x10000];
 } p6502;
 
 static void 
 stack_push(void)
 {
-	p6502.memory[0x100 | p6502.SP--] = p6502.A;
+	p6502.memory[0x100 | p6502.reg.SP--] = p6502.reg.A;
 }
 static void 
 stack_pull(void)
 {
-	p6502.A = p6502.memory[0x100 | ++p6502.SP];
+	p6502.reg.A = p6502.memory[0x100 | ++p6502.reg.SP];
 }
