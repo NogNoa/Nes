9f32e1a734aaffc55ce68803616e666bd73c82dd
NogNoa <nognavon@gmail.com>
2024-08-28 15:17:27 +0300

    restructure address decoder

--- mainboard.c
+++ mainboard.c
@@ -6,14 +6,12 @@ void adress_decoder(uint16_t address)
     uint8_t control = address >> 13;
     if (control & 0b100)
         {return romsel(address & (('\1' << 15) - 1));}
+    else if (control & 0b10)
+        {return;}
+    else if (control & 1)
+        {return ppu_cs(address & (('\1' << 3) - 1));}
     else
-    {   if (control & 0b10)
-            {return;}
-        else if (control & 1)
-            {return ppu_cs(address & (('\1' << 3) - 1));}
-        else
-            {return cpu_ram_cs(address & (('\1' << 11) - 1));}
-    }
+        {return cpu_ram_cs(address & (('\1' << 11) - 1));}
 }
 
 uint8_t demultiplexer_74139(bool enable, uint8_t address)
