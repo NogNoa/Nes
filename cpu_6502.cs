5ad845111097a43d2c7b6d3a0c7d18aa4b967f2e
NogNoa <nognavon@gmail.com>
2024-08-31 15:59:44 +0300

    start a dotnet project

--- cpu_6502.cs
+++ cpu_6502.cs
@@ -5,46 +5,46 @@ class cpu_6502
     byte Y;
     byte SP;
     byte P;
-    uint16 PC;
+    short PC;
 
-    public uint16 address;
+    public short address;
     public byte data;
     
 
     byte bit_set(bool value, byte the_byte, byte power_o_2)
     {
-        return (value) ? the_byte | power_o_2 : the_byte & ~power_o_2;
+        return (byte) ((value) ? the_byte | power_o_2 : the_byte & ~power_o_2);
     }
 
     bool carry
-    {   get => (bool) (this.P & 1);
+    {   get => (this.P & 1) != 0;
         set {this.P = bit_set(value, this.P, 1);}
     }
     bool zero
-    {   get => (bool) (this.P & 0b10);
+    {   get => (this.P & 0b10) != 0;
         set {this.P = bit_set(value, this.P, 0b10);}
     }
     bool interrupt_disable 
-    {   get => (bool) (this.P & 0b100);
+    {   get => (this.P & 0b100) != 0;
         set {this.P = bit_set(value, this.P, 0b100);}
     }
     bool mode_decimal 
-    {   get => (bool) (this.P & 0b1000);
+    {   get => (this.P & 0b1000) != 0;
         set {this.P = bit_set(value, this.P, 0b1000);}
     }
     bool overflow 
-    {   get => (bool) (this.P & 0b100000);
+    {   get => (this.P & 0b100000) != 0;
         set {this.P = bit_set(value, this.P, 0b100000);}
     }
     bool negative 
-    {   get => (bool) (this.P & 0b1000000);
+    {   get => (this.P & 0b1000000) != 0;
         set {this.P = bit_set(value, this.P, 0b1000000);}
     }
 
 
     private enum interrupt_vector {NMI=0xFFFA, RST=0xFFFC, IRQ=0xFFFE};
 
-    void interrupt(uint16 vector)
+    void interrupt(short vector)
     {   this.push((byte)(this.PC >> 8));
         this.push((byte)(this.PC));
         this.push(this.P);
@@ -54,17 +54,17 @@ class cpu_6502
 
     public void interrupt_request() 
     {   if (this.interrupt_disable)
-        {   interrupt(interrupt_vector.IRQ);
+        {   interrupt((short) interrupt_vector.IRQ);
         }
     }
 
     public void nonmaskable_interrupt() 
-    {   interrupt(interrupt_vector.NMI);
+    {   interrupt((short) interrupt_vector.NMI);
     }
 
     public void reset()
     {   this.interrupt_disable = true;
-        this.PC = interrupt_vector.RST;
+        this.PC = (short) interrupt_vector.RST;
     }
     
     public void set_overflow() {this.overflow = true;}
