f75ec93e18bb38b5a598439166bf772052caca3a
NogNoa <nognavon@gmail.com>
2024-08-31 15:44:03 +0300

    use bitset and interrupt helpers

--- cpu_6502.cs
+++ cpu_6502.cs
@@ -1,48 +1,71 @@
 class cpu_6502
 {
-    uint8 A;
-    uint8 X;
-    uint8 Y;
-    uint8 SP;
-    uint8 P;
+    byte A;
+    byte X;
+    byte Y;
+    byte SP;
+    byte P;
     uint16 PC;
+
+    public uint16 address;
+    public byte data;
+    
+
+    byte bit_set(bool value, byte the_byte, byte power_o_2)
+    {
+        return (value) ? the_byte | power_o_2 : the_byte & ~power_o_2;
+    }
 
     bool carry
     {   get => (bool) (this.P & 1);
-        set {(value) ? this.P | 1 : this.P & -2;}
+        set {this.P = bit_set(value, this.P, 1);}
     }
     bool zero
-    {   get => (bool) (this.P & 2);
-        set {(value) ? this.P | 2 : this.P & -3;}
+    {   get => (bool) (this.P & 0b10);
+        set {this.P = bit_set(value, this.P, 0b10);}
     }
     bool interrupt_disable 
-    {   get => (bool) (this.P & 4);
-        set {(value) ? this.P | 4 : this.P & -5;}
+    {   get => (bool) (this.P & 0b100);
+        set {this.P = bit_set(value, this.P, 0b100);}
     }
     bool mode_decimal 
-    {   get => (bool) (this.P & 8);
-        set {(value) ? this.P | 8 : this.P & -9;}
+    {   get => (bool) (this.P & 0b1000);
+        set {this.P = bit_set(value, this.P, 0b1000);}
     }
-    bool overflow => (bool) (this.P & 0b100000);
-    bool negative => (bool) (this.P & 0b1000000);
+    bool overflow 
+    {   get => (bool) (this.P & 0b100000);
+        set {this.P = bit_set(value, this.P, 0b100000);}
+    }
+    bool negative 
+    {   get => (bool) (this.P & 0b1000000);
+        set {this.P = bit_set(value, this.P, 0b1000000);}
+    }
 
 
     private enum interrupt_vector {NMI=0xFFFA, RST=0xFFFC, IRQ=0xFFFE};
 
-    void interrupt_request() 
+    void interrupt(uint16 vector)
+    {   this.push((byte)(this.PC >> 8));
+        this.push((byte)(this.PC));
+        this.push(this.P);
+        this.interrupt_disable = true;
+        this.PC = vector;
+    }
+
+    public void interrupt_request() 
     {   if (this.interrupt_disable)
-        {   this.push((uint8)(this.PC >> 8));
-            this.push((uint8)(this.PC));
-            this.push(this.P);
-            this.interrupt_disable = true; 
-            this.push(this.A);
-            PC = interrupt_vector.IRQ;
+        {   interrupt(interrupt_vector.IRQ);
         }
     }
 
-    void nonmaskable_interrupt() 
-    {   this.push(this.P);
-        this.push(this.A);
-        PC = interrupt_vector.NMI;
+    public void nonmaskable_interrupt() 
+    {   interrupt(interrupt_vector.NMI);
     }
+
+    public void reset()
+    {   this.interrupt_disable = true;
+        this.PC = interrupt_vector.RST;
+    }
+    
+    public void set_overflow() {this.overflow = true;}
 }
