0b28701e9b0f437cd2bb3eb43615addfb63aa09b
NogNoa <nognavon@gmail.com>
2024-09-05 18:10:46 +0300

    define recipients by argument list

--- nes_board.cs
+++ nes_board.cs
@@ -1,8 +1,15 @@
 class NesBoard:IBus
 {
     readonly CPU2403 cpu;
+    readonly Demultiplxer[] address_decoder;
+    public NesBoard()
+    {
+        this.cpu = new CPU2403(this, new Controller[2]);
+        this.address_decoder = new Demultiplxer[2];
+        this.address_decoder[1] = new Demultiplxer(new RAM, new Cartridge, new DeadEnd, new DeadEnd);
+    }
 
-    byte Access(ushort address, byte value, ReadWrite readWrite)
+    public byte Access(ushort address, byte value, ReadWrite readWrite)
     {return 0;}
 
 }
@@ -11,12 +18,18 @@ class Demultiplxer
 {
     readonly IBus[] recipients;
 
-    Demultiplxer(IBus[] recipients)
+    public Demultiplxer(IBus r0, IBus r1, IBus r2, IBus r3)
     {
-        this.recipients = recipients;
+        this.recipients = [r0, r1, r2, r3];
     }
-    byte Access(ushort address, byte value, ReadWrite readWrite)
+    public byte Access(byte index, ushort address, byte value, ReadWrite readWrite)
     {
-        recipients[]
+        return recipients[index].Access(address, value, readWrite);
     }
+}
+
+class DeadEnd: IBus
+{
+    public byte Access(ushort address, byte value, ReadWrite readWrite)
+    {return value;}
 }
